9a576b530f5f6760ae44a453a0d9f9370169e74a
Marius Preikschat <m.preikschat@googlemail.com>
2024-11-26 14:36:29 +0100

    Initial Commit

---
+++ Intergalaktusse/IntergalaktusseView.h
@@ -0,0 +1,12 @@
+//
+//  IntergalaktusseView.h
+//  Intergalaktusse
+//
+//  Created by Marius Preikschat on 26.11.24.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface IntergalaktusseView : ScreenSaverView
+
+@end
---
+++ Intergalaktusse/IntergalaktusseView.m
@@ -0,0 +1,51 @@
+//
+//  IntergalaktusseView.m
+//  Intergalaktusse
+//
+//  Created by Marius Preikschat on 26.11.24.
+//
+
+#import "IntergalaktusseView.h"
+
+@implementation IntergalaktusseView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
